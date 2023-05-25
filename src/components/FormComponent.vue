940cfb08ed59d12c7f29f20d35088aff776a1110
pujan <pujxnp@gmail.com>
2023-05-25 10:37:18 +0530

    Added mandatory asterisks to i/p fields

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -41,24 +41,27 @@ const allFieldsFilled = () => {
     <div class="mt-8 flex flex-col items-center justify-center">
         <div class="w-80">
             <form id="userForm" class="flex flex-col space-y-2 border-4 border-light-orange p-4">
-                <label class="text-light-orange font-bold">Latitude:<br /><input class="text-black font-normal w-full"
-                        v-model.number="latVal" placeholder="Enter Latitude" /></label>
+                <label class="text-light-orange font-bold"><span class="text-red-400">* </span>Latitude:<br /><input
+                        class="text-black font-normal w-full" v-model.number="latVal"
+                        placeholder="Enter Latitude" /></label>
                 <div v-if="isLatError()" class="text-red-400 text-sm">
                     <span class="material-icons inline-block align-middle">error</span><span>Latitude must be a number
                         between -90
                         and 90</span>
                 </div>
 
-                <label class="text-light-orange font-bold">Longitude:<br /><input class="text-black font-normal w-full"
-                        v-model.number="longVal" placeholder="Enter Longitude" /></label>
+                <label class="text-light-orange font-bold"><span class="text-red-400">* </span>Longitude:<br /><input
+                        class="text-black font-normal w-full" v-model.number="longVal"
+                        placeholder="Enter Longitude" /></label>
                 <div v-if="isLongError()" class="text-red-400 text-sm">
                     <span class="material-icons inline-block align-middle">error</span><span class="text-red-400">Longitude
                         must be a number between -180 and 179.99</span>
                 </div>
-                <label class="text-light-orange font-bold">Place name:<br /><input class="text-black font-normal w-full"
-                        v-model="placeNameVal" required placeholder="Enter Name" /></label>
-                <label class="text-light-orange font-bold">Place description:<br /><input
-                        class="text-black font-normal w-full" v-model="placeDescVal" required
+                <label class="text-light-orange font-bold"><span class="text-red-400">* </span>Place name:<br /><input
+                        class="text-black font-normal w-full" v-model="placeNameVal" required
+                        placeholder="Enter Name" /></label>
+                <label class="text-light-orange font-bold"><span class="text-red-400">* </span>Place
+                    description:<br /><input class="text-black font-normal w-full" v-model="placeDescVal" required
                         placeholder="Enter description" /></label>
             </form>
         </div>
